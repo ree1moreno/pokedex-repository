96c0b48edb08b23870668791ecd87143644b91f6
Renato Moreno <renats.mc@gmail.com>
2022-03-27 02:43:15 -0300

    adiciona contexto inicial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import PokemonProvider from './context/PokemonProvider';
 import Main from './pages/Main';
 
 function App() {
   return (
-    // <PokemonProvider>
-    <Routes>
-      <Route exact path="" element={<Main />}/>
-    </Routes>
-    // </PokemonProvider>
+    <PokemonProvider>
+      <Routes>
+        <Route exact path="" element={<Main />}/>
+      </Routes>
+   </PokemonProvider>
   );
 }
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import PokemonContext from '../context/PokemonContext';
 
 export default function Main() {
+  const { pokemonList } = React.useContext(PokemonContext);
+
   return (
     <div>
-      Pokedex
+      {/* {!pokemonList.length ? <p>carregando</p> : <p>{pokemonList[0].entry_number}</p>} */}
     </div>
   )
 }
