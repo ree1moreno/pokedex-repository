ef92eb1cbd18f8153ddc7f45fee799ab889b5bdb
Renato Moreno <renats.mc@gmail.com>
2022-03-27 03:01:36 -0300

    adiciona componentes globais

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>Desenvolvido por <a href='https://www.linkedin.com/in/reemoreno/' target='_blank' rel='noreferrer'>Renato Moreno</a>.</p>
+    </footer>
+  )
+}
